82589febd32a0e570c2f26570384c09e3587e9fa
Choe Choon Ho <choechoonho@gmail.com>
2018-02-13 22:44:38 +0800

    Modified concordance.js, example.html.
    Added access identifier in concordance.js.
    Changed example.html to reflect the changes.

--- Concordance/concordance.js
+++ Concordance/concordance.js
@@ -1,13 +1,4 @@
-function concordance(text, data) {
-
-	/* Assumes data.length is more than 0. */
-	if (data.length < 1) {
-		return;
-	}
-
-	/* Assumes data keys are "sentence" and "sentiment". */
-	var sentence = Object.keys(data[0])[0];
-	var sentiment = Object.keys(data[0])[1];
+function concordance(text, data, sentence_access = null, sentiment_access = null) {
 
 	// Creates "modal dialog".
 	var modal = document.createElement('div');
@@ -25,47 +16,53 @@ function concordance(text, data) {
 	var concordance = document.createElement('table');
 	concordance.className = "modal-table";
 
-	for (var i = 0; i < data.length; i ++) {
+	if (sentence_access != null)
+	{
+		for (var i = 0; i < data.length; i ++) {
 
-		var str = data[i][sentence];
+			var str = sentence_access(data[i]);
 
-		// Find text.
-		var n = str.search(text);
+			// Find text.
+			var n = str.search(text);
 
-		if (n === -1) {
-			continue;
+			if (n === -1) {
+				continue;
+			}
+
+			// Creates new entry.
+			var row = document.createElement('tr');
+
+			var left = document.createElement('td');
+			var center = document.createElement('td');
+			var right = document.createElement('td');
+
+			// Alignment.
+			left.className = 'modal-text-left';
+			center.className = 'modal-text-center';
+			right.className = 'modal-text-right';
+
+			// Text.
+			left.innerHTML = str.substr(0, n);
+			center.innerHTML = text;
+			right.innerHTML = str.substr(n + text.length);
+
+			// Color.
+			if (sentiment_access != null)
+			{
+				if (sentiment_access(data[i])) {
+					center.className += " positive";
+				}
+				else {
+					center.className += " negative";
+				}
+			}
+
+			row.appendChild(left);
+			row.appendChild(center);
+			row.appendChild(right);
+
+			concordance.appendChild(row);
 		}
-
-		// Creates new entry.
-		var row = document.createElement('tr');
-
-		var left = document.createElement('td');
-		var center = document.createElement('td');
-		var right = document.createElement('td');
-
-		// Alignment.
-		left.className = 'modal-text-left';
-		center.className = 'modal-text-center';
-		right.className = 'modal-text-right';
-
-		// Text.
-		left.innerHTML = str.substr(0, n);
-		center.innerHTML = text;
-		right.innerHTML = str.substr(n + text.length);
-
-		// Color.
-		if (data[i][sentiment]) {
-			center.className += " positive";
-		}
-		else {
-			center.className += " negative";
-		}
-
-		row.appendChild(left);
-		row.appendChild(center);
-		row.appendChild(right);
-
-		concordance.appendChild(row);
 	}
 
 	content.appendChild(close);
