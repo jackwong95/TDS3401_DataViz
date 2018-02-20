ed401af096695a6ba6d154af94621458cec24ff5
DESKTOP-8FBNPUA\Wong Tiong Kiat <jackwongtiongkiat@gmail.com>
2018-02-20 17:25:25 +0800

    fixed bug where a new svg is created every time for every new query

--- Barchart/barchart.js
+++ Barchart/barchart.js
@@ -1,14 +1,27 @@
 //http://bl.ocks.org/enjalot/1429426
 function create_barchart(data, chart_sz, svg_id = "svg_bar", margin = {left:0, top:0, right:0, bottom:0}, graph_properties = {colors:["#11AA00", "#C70039"], show:40, order:"DSC"}, onclick = null)
 {
-	var svg = d3.select("body")
-		.append("svg")
-		.attr("id", svg_id)
+    // setup the svg
+    var svg = d3.select("#svg_barchart")
 		.attr("width", chart_sz.width + margin.left + margin.right)
-		.attr("height", chart_sz.height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.attr("height", chart_sz.height + margin.top + margin.bottom);
 	
+	// remove graph that still exists
+	var svg_g = d3.selectAll(".bar_svg_g");
+	if (!svg_g.empty())
+	{
+		svg_g.remove();
+	}
+	
+	svg_g = svg.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.attr("class", "bar_svg_g");
+	
+	init(data, chart_sz, svg_g, margin, graph_properties, onclick);
+}
+
+function init(data, chart_sz, svg, margin = {left:0, top:0, right:0, bottom:0}, graph_properties = {colors:["#11AA00", "#C70039"], show:40, order:"DSC"}, onclick = null)
+{
 	data.sort(function(a, b)
 	{ 
 		var a = parseInt(a.positive) + parseInt(a.negative);
@@ -16,7 +29,7 @@ function create_barchart(data, chart_sz, svg_id = "svg_bar", margin = {left:0, t
 		return (graph_properties.order=="ASC"?(a - b):(b - a));
 	});
 	
-	data = data.slice(1, graph_properties.show+1);
+	data = data.slice(0, graph_properties.show);
 	
 	// Transpose the data into layers
 	var dataset = d3.layout.stack()(["positive", "negative"].map(function(sentiment)
@@ -36,7 +49,6 @@ function create_barchart(data, chart_sz, svg_id = "svg_bar", margin = {left:0, t
 		.domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
 		.range([chart_sz.height, 0]);
 
-
 	// Define and draw axes
 	var yAxis = d3.svg.axis()
 		.scale(y)
@@ -50,43 +62,150 @@ function create_barchart(data, chart_sz, svg_id = "svg_bar", margin = {left:0, t
 		.orient("bottom");
 
 	svg.append("g")
-		.attr("class", "y axis")
+		.attr("class", "y axis bar_y_axis")
 		.call(yAxis);
 
+	
 	svg.append("g")
-		.attr("class", "x axis")
 		.attr("transform", "translate(0," + chart_sz.height + ")")
+		.attr("class", "y axis bar_x_axis")
 		.call(xAxis);
-	
-	var vis = d3.select("#barchart");
-	
+
 	// Create groups for each series, rects for each segment 
 	var groups = svg.selectAll("g.cost")
 		.data(dataset)
 		.enter().append("g")
 		.attr("class", "cost")
-		.style("fill", function(d, i) { return graph_properties.colors[i]; });
-
-	// Prep the tooltip bits, initial display is hidden
-	var tooltip = svg.append("g")
-		.attr("class", "tooltip")
-		.style("display", "none");
-		
-	tooltip.append("rect")
-		.attr("width", 30)
-		.attr("height", 20)
-		.attr("fill", "white")
-		.style("opacity", 0.5);
-
-	tooltip.append("text")
-		.attr("x", 15)
-		.attr("dy", "1.2em")
-		.style("text-anchor", "middle")
-		.attr("font-size", "12px")
-		.attr("font-weight", "bold");
+		.style("fill", function(d, i) { return graph_properties.colors[i]; });	
 
 	var bars = groups.selectAll(".bar_rect")
 		.data(function(d) { return d; });
+		
+	// enter
+	bars.enter()
+		.append("rect")
+		.attr("class", "bar_rect")
+		.attr("x", function(d) { return x(d.x); })
+		.attr("y", function(d) { return y(d.y0 + d.y); })
+		.attr("height", 0)
+		.attr("width", x.rangeBand())
+		.on("mouseover", function() { tooltip.style("display", null); })
+		.on("mouseout", function() { tooltip.style("display", "none"); })
+		.on("mousemove", function(d) 
+			{
+				var xPosition = d3.mouse(this)[0] - 15;
+				var yPosition = d3.mouse(this)[1] - 25;
+				tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+				tooltip.select("text").text(d.y);
+			}
+		)
+		.on("click", function (d)
+			{
+				if (onclick)
+				{
+					onclick(d.x);
+				}
+			}
+		)
+		.transition()
+		.duration(1500)
+		.delay(function(d, i) { return i * 250; }) // different delay for each bar
+		.attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+		
+	// Prep the tooltip bits, initial display is hidden
+	
+	var tooltip = d3.select(".tooltip");
+	if (tooltip.empty())
+	{
+		tooltip = svg.append("g")
+		.attr("class", "tooltip")
+		.style("display", "none");
+	
+		tooltip.append("rect")
+			.attr("width", 30)
+			.attr("height", 20)
+			.attr("fill", "white")
+			.style("opacity", 0.5);
+
+		tooltip.append("text")
+			.attr("x", 15)
+			.attr("dy", "1.2em")
+			.style("text-anchor", "middle")
+			.attr("font-size", "12px")
+			.attr("font-weight", "bold");
+	}
+		
+	/*
+	// Create groups for each series, rects for each segment 
+	var groups = svg.selectAll("g.cost")
+		.data(dataset);
+	
+	console.log("dataset : " + Object.keys(dataset));
+	
+	console.log(groups.enter());
+	console.log(groups.exit());
+	
+	// enter
+	groups.enter()
+		.append("g")
+		.attr("class", "cost")
+		//.attr("test", function(d){console.log("entering : " + d); })
+		.style("fill", function(d, i) { return graph_properties.colors[i]; })
+			.selectAll(".bar_rect")
+			.data(function(d) { return d; })
+			.enter()
+				.append("rect")
+				.attr("class", "bar_rect")
+				.attr("x", function(d) { return x(d.x); })
+				.attr("y", function(d) { return y(d.y0 + d.y); })
+				.attr("height", 0)
+				.attr("width", x.rangeBand())
+				.on("mouseover", function() { tooltip.style("display", null); })
+				.on("mouseout", function() { tooltip.style("display", "none"); })
+				.on("mousemove", function(d) 
+					{
+						var xPosition = d3.mouse(this)[0] - 15;
+						var yPosition = d3.mouse(this)[1] - 25;
+						tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+						tooltip.select("text").text(d.y);
+					}
+				)
+				.on("click", function (d)
+					{
+						if (onclick)
+						{
+							onclick(d.x);
+						}
+					}
+				)
+				.transition()
+				.duration(1500)
+				.delay(function(d, i) { return i * 250; }) // different delay for each bar
+				.attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+	*/
+	// exit
+	//console.log(groups.exit());
+	/*
+	groups.exit()
+		.attr("test", function(d){console.log(d);})
+		.remove();
+		*/
+	/*
+		.enter().append("g")
+		.attr("class", "cost")
+		.style("fill", function(d, i) { return graph_properties.colors[i]; });
+
+	var bars = groups.selectAll(".bar_rect")
+		.data(function(d) { return d; });
+
+	// exit
+	bars.exit()
+		.transition()
+		.duration(300)
+		.attr("y", y(0))
+		.attr("height", chart_sz.height - y(0))
+		.style("fill-opacity", 1e-6)
+		.remove();
 		
 	// update	
 	bars.attr("x", function(d) { return x(d.x); })
@@ -126,17 +245,15 @@ function create_barchart(data, chart_sz, svg_id = "svg_bar", margin = {left:0, t
 		.duration(1500)
 		.delay(function(d, i) { return i * 250; }) // different delay for each bar
 		.attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
-
-	// exit
-	bars.exit()
-		.transition()
-		.duration(300)
-		.attr("y", y(0))
-		.attr("height", chart_sz.height - y(0))
-		.style("fill-opacity", 1e-6)
-		.remove();
-		
+	*/
 	// Draw legend
+	var legend = svg.selectAll(".bar_legend");
+	
+	if (!legend.empty())
+	{
+		legend.remove();
+	}
+	
 	var legend = svg.selectAll(".bar_legend")
 		.data(graph_properties.colors)
 		.enter().append("g")
@@ -162,4 +279,5 @@ function create_barchart(data, chart_sz, svg_id = "svg_bar", margin = {left:0, t
 				case 1: return "Positive";
 			}
 		});
+	
 }
